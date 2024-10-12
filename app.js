e556f406a7a40e45d40b73d00a94dc6be310a0a2
shahzod <shoxzodbek15@gmail.com>
2024-10-12 15:30:07 +0500

    open project

---
+++ app.js
@@ -0,0 +1,10 @@
+function getNamesByGrade(students, grade){
+    const students = [
+        { name: "Quincy", percent: 96 },
+        { name: "Jason", percent: 84 },
+        { name: "Alexis", percent: 100 },
+        { name: "Sam", percent: 65 },
+        { name: "Katie", percent: 90 },
+        { name: "Anna", percent: 75 },
+            
+}
